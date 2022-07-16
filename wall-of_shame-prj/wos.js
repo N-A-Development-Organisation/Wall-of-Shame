9ba18cda0c7f9f7d41c24d8ef055b15005044bc5
Ahmedsassi1 <ahmed_sassi92@hotmail.com>
2022-07-16 05:12:23 +0200

    html and js done

--- wall-of_shame-prj/wos.js
+++ wall-of_shame-prj/wos.js
@@ -35,51 +35,102 @@ function createNote (name) {
         addBlue : addBlue,
     }
 }
+var notes = [];
+var display = function(){
+    var disp = '';
+    for(var i = 0 ; i<notes.length ; i++){
+    disp = notes[i].name + ' note have '+notes[i].NRed+' red pins '+ notes[i].NBlue +' blue pins '+notes[i].NYellow+" yellow pins<br>" + disp
+    }
+    $("#Stats").html("<div>"+disp+"</div>")
+}
+
 
 var addRed = function addRed(){
     this.NRed = this.NRed +1
     return this.NRed ;
+    display();
 }
 var addYellow = function addYellow(){
     this.NYellow = this.NYellow +1 ;
     return this.NYellow ;
+    display();
 }
 var addBlue = function addBlue(){
     this.NBlue = this.NBlue +1 ;
     return this.NBlue;
+    display();
 }
 
 var removeRed = function removeRed(){
     this.NRed = this.NRed -1;
     return this.NRed ;
+    display();
 }
 var removeYellow = function removeYellow(){
     this.NYellow = this.NYellow -1;
     return this.NYellow ;
+    display();
 }
 var removeBlue = function removeBlue(){
     this.NBlue = this.NBlue -1;
     return this.NBlue ;
+    display();
 }
-var notes = [];
-var display = function(){
-    for(var i = 0 ; i<notes.length ; i++){
-        console.log (notes[i].name + ' note have '+notes[i].NRed+' red pins '+ notes[i].NBlue +' blue pins '+notes[i].NYellow+' yellow pins' )
-    }
-}
+
 
 $("#Noteadd").on("click",function(){ 
 	var NoteName= $("#inputName").val()
 	if (NoteName === ''){
         alert(" Enter a name please");
 	}
-    else var a =createNote(NoteName)
-        notes.push(a)
+    else {var a =createNote(NoteName)
+        notes.push(a)};
+        display();
+        displayONBoard2();
+         $("#Manage").append("<p>"+NoteName+"</p>").append("<p>:  is added </p><br>")
+	});
 
-	}
-);
+var notes = [];
 
-$("#board").append(('<div id='stats'></div>'));
-$("#stats").html('display()')
+var displayONBoard2 = function(){
+    var disp = '';
+    for(var i = 0 ; i<notes.length ; i++){
+    disp ="<div><h1>" +notes[i].name +'<h1>'+notes[i].NRed+'<br>'+ notes[i].NBlue +'<br>'+notes[i].NYellow+"<br></div><br><br>" + disp
+    }
+    $("#Board").html("<div>"+disp+"</div>")
+}
 
 
+$("#addRedpin").on("click",function(){
+    if (notes[0]=== undefined){
+        alert(" Put someone on shame ");
+    }
+    var NameInred = $("#NameOnNote1").val()
+    if (NameInred === ''){
+        alert(" Add someone please ")
+    }
+    for (var i = 0; i < notes.length; i++) {
+        if (NameInred === notes[i].name) {
+            notes[i].addRed();
+            display();
+            displayONBoard2()
+        } 
+    }
+})
+
+$("#removeRedpin").on("click",function(){
+    if (notes[0]=== undefined){
+        alert(" Put someone on shame ");
+    }
+    var NameInred = $("#NameOnNote1").val()
+    if (NameInred === ''){
+        alert(" Add someone please ")
+    }
+    for (var i = 0; i < notes.length; i++) {
+        if (NameInred === notes[i].name) {
+            notes[i].removeRed();
+            display();
+            displayONBoard2()
+        } 
+    }
+})
